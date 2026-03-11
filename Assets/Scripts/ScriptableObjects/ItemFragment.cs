71b3d42301d44abd23e1cb1b59ef3ad67da18c53
Ruth Sun <rsun050@ucr.edu>
2026-03-10 18:56:15 -0700

    add scriptableobjs for dialogues and quests, write up quest dialogue

---
+++ Assets/Scripts/ScriptableObjects/ItemFragment.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// any sort of npc interaction that changes the user's inventory
+[CreateAssetMenu(fileName = "Item Fragment", menuName = "ScriptableObjects/Item Fragment", order = 1)]
+public class ItemFragment : Fragment
+{
+	public List<ItemData> items;
+	public List<int> quantityChange;
+}
---
+++ Assets/Scripts/ScriptableObjects/NPCData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC Data", menuName ="ScriptableObjects/NPC", order = 1)]
+public class NPCData : ScriptableObject
+{
+	public string NPCName;
+}
