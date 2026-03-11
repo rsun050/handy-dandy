6d0b2a43f5abb56a81cd80dbe5af4ca935d367d0
Ruth Sun <rsun050@ucr.edu>
2026-03-10 17:31:29 -0700

    refactor apple_script slightly, prefab apples

--- Assets/Michael_Folder/Imports/Apple_Script.cs
+++ Assets/Michael_Folder/Imports/Apple_Script.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Apple_Script : MonoBehaviour, I_Interactble_Michael
 {
     [SerializeField] private Transform myTransform;
+    [SerializeField] private ItemData itemData;
 
     public string GetInteractText()
     {
-        return "Pick Up Apple";
+        return $"Pick Up {itemData.ItemName}";
     }
 
     public void OnInteract()
     {
-        Debug.Log("Yippie you picked up the Apple");
+        Debug.Log($"Yippie you picked up the {itemData.ItemName}");
         Player_Held_Item_Manager.Instance.Grab_Held_Item(myTransform);
     }
 }
