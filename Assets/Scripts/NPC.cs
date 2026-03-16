7151a4ab5faac64f953357cef057766ab496150e
Ruth Sun <rsun050@ucr.edu>
2026-03-16 16:25:40 -0700

    mod ruth scene decorations, 2 new NPCs (for flavour.)

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -13,9 +13,22 @@ public class NPC : MonoBehaviour
 	[SerializeField] private DialogueFragment[] questCompleteDialogues; // quest completed
 	[SerializeField] private DialogueFragment[] defaultDialogue; // miscellaneous dialogue, unimportant
 	[SerializeField] private AudioSource talkSfx;
-	private void Awake()
-	{
+	[SerializeField] private Animator animator;
+	private void Awake() {
 		
+	}
+
+	private void Start() {
+		UpdateQuestIcon();
+		if(animator) {animator.SetBool("talking", false); }
+	}
+
+	private void UpdateQuestIcon() {
+		if(quest >= quests.Length) {
+			transform.GetChild(0).gameObject.SetActive(false);
+		} else {
+			transform.GetChild(0).gameObject.SetActive(true);
+		}
 	}
 
 	private void Update()
@@ -33,43 +46,41 @@ public class NPC : MonoBehaviour
 		QuestManager.Instance.questAcceptedE -= questaccepted;
 	}
 
+	private void StopYapAnim() {
+		animator.SetBool("talking", false);
+
+		DialogueManager.Instance.dialogueEndE -= StopYapAnim;
+	}
+
 	public void Interact() {
-		if(quests.Length == 0 && defaultDialogue.Length == 0) {
-			// this NPC has no dialogue!
-			// Debug.Log("This NPC has no dialogue");
+		if(quests.Length == 0 && defaultDialogue.Length == 0) { // this NPC has no dialogue!
 			return;
 		}
 
 		talkSfx.Play();
 
-		if(quest >= quests.Length) {
-			// all quests exhausted, fallback to default dialogues
-			// Debug.Log("NPC dialogue exhausted, using default");
+		if(animator != null) {
+			animator.SetBool("talking", true );
+			DialogueManager.Instance.dialogueEndE += StopYapAnim;			
+		}
+
+		if(quest >= quests.Length) { // all quests exhausted, fallback to default dialogues
 			if(defaultDialogue.Length > 0) {
 				// play a random greeting
 				int i = Random.Range(0, defaultDialogue.Length);
 				DialogueManager.Instance.StartDialogue(defaultDialogue[i]);
 			}
-		}
-		else if(!questIP) {
-			// start a new quest
-			// Debug.Log("Offering quest");
-			// isOfferingQuest = true;
+		} else if(!questIP) { // start a new quest
 			QuestManager.Instance.questAcceptedE += questaccepted;
 			DialogueManager.Instance.StartDialogue(questStartDialogues[quest]);
-		} else { // questIP
-			// quest is in progress: check if the quest is complete
-			if(QuestManager.Instance.questIsComplete()) {
-				// play questCompleteDialogue
-				// Debug.Log("Quest completed");
-
+		} else { // quest in progress, check if the quest is complete
+			if(QuestManager.Instance.questIsComplete()) { // quest is complete
 				QuestManager.Instance.CompleteQuest();
 				DialogueManager.Instance.StartDialogue(questCompleteDialogues[quest]);
 				questIP = false;
 				quest += 1;
-			} else {
-				// Debug.Log("Quest IP, not complete");
-				// play questIPDialogue
+				UpdateQuestIcon();
+			} else { // quest NOT complete
 				DialogueManager.Instance.StartDialogue(questIPDialogues[quest]);
 			}
 		}
